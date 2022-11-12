a4f785fcb4d0edb1c8e012c10248d5169545e68e
Azat Gilyazov <57267956+AnteeOne@users.noreply.github.com>
2022-11-12 21:00:39 +0300

    feature: init project

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+    java
+}
+
+group = "tech.antee"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+    implementation("org.postgresql:postgresql:42.3.0")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.69")
+    implementation("org.projectlombok:lombok:1.18.20")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
